203ae70a99ce25e52855c0a510a0c5cb6554e6f4
SatishThota16 <satishnaidu3541@gmail.com>
2026-08-31 20:29:13 +0530

    feat(easy): add CHEFWORK solution [Java]

---
+++ codechef/easy/CHEFWORK---Rating-1146/solution.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] c = new int[n];
+        int[] t = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            c[i] = scanner.nextInt();
+        }
+
+        for (int i = 0; i < n; i++) {
+            t[i] = scanner.nextInt();
+        }
+
+        // Your code goes here
+        int one=Integer.MAX_VALUE;
+        int two=Integer.MAX_VALUE;
+        int three=Integer.MAX_VALUE;
+        
+        for(int i=0;i<n;i++)
+        {
+            if(t[i]==1)
+            {
+                if(c[i]<one)
+                {
+                    one=c[i];
+                }
+            }
+           else if(t[i]==2)
+            {
+                if(c[i]<two)
+                {
+                    two=c[i];
+                }
+            }
+            else{
+                if(c[i]<three)
+                {
+                    three=c[i];
+                }
+            }
+            
+        }
+        
+        if(three<(one+two))
+        {
+            System.out.println(three);
+        }
+        else{
+            int result=one+two;
+            System.out.println(result);
+        }
+
+        
+        
+    }
+}
