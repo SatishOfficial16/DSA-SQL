88a0c2a855851726d062e61a5b92ca6ecdf4c4bc
SatishThota16 <satishnaidu3541@gmail.com>
2026-09-11 01:50:34 +0530

    feat(easy): add KTTABLE solution [Java]

---
+++ codechef/easy/KTTABLE---Rating-997/solution.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes 
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0)
+		{
+		    int n=sc.nextInt();
+		    int a[]=new int[n];
+		    int b[]=new int[n];
+		    for(int i=0;i<n;i++)
+		    {
+		        a[i]=sc.nextInt();
+		        b[i]=sc.nextInt();
+		    }
+		    
+		    int count=0;
+		    if(a[0]>=b[0])
+		    {
+		        count++;
+		    }
+		    
+		    for(int i=1;i<n;i++)
+		    {
+		        if((a[i-1]-a[i])>= b[i])
+		        {
+		            count++;
+		        }
+		    }
+		    
+		    System.out.println(count);
+		   
+		}
+		
+		
+
+	}
+}
