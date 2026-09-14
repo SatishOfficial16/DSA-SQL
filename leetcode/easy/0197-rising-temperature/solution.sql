0563bb1ffadfe6d78f5dcbe37cc75fcd9367f0f1
SatishThota16 <satishnaidu3541@gmail.com>
2026-09-14 14:39:31 +0530

    feat(easy): add rising-temperature solution [SQL]

--- leetcode/easy/0197-rising-temperature/solution.sql
+++ leetcode/easy/0197-rising-temperature/solution.sql
@@ -1,5 +1,5 @@
 select w.id
 from Weather w
 join weather w2
-on datediff(w.recordDate,w2.recordDate)=1
-where w.temperature - w2.temperature >0
+on w.recordDate=date_add(w2.recordDate,interval 1 day)
+where w.temperature> w2.temperature
