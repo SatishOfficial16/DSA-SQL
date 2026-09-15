524636daa1a783f5e73b4f6ce8fae65066b31f07
SatishThota16 <satishnaidu3541@gmail.com>
2026-09-15 18:54:16 +0530

    feat(easy): add number-of-unique-subjects-taught-by-each-teacher solution [SQL]

--- leetcode/easy/2356-number-of-unique-subjects-taught-by-each-teacher/solution.sql
+++ leetcode/easy/2356-number-of-unique-subjects-taught-by-each-teacher/solution.sql
@@ -1,7 +1,3 @@
-# Write your MySQL query statement below
-select teacher_id,count(subject_id)as cnt
-from
-(select teacher_id ,subject_id
-from Teacher
-group by teacher_id,subject_id )as temp
+select teacher_id,count(distinct subject_id)as cnt
+from teacher 
 group by teacher_id
