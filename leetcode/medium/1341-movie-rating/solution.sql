e44947c3a07dddbaf2c335715f5cc6b457c3d9cf
SatishThota16 <satishnaidu3541@gmail.com>
2026-09-15 19:59:24 +0530

    feat(medium): add movie-rating solution [SQL]

---
+++ leetcode/medium/1341-movie-rating/solution.sql
@@ -0,0 +1,24 @@
+(SELECT u.NAME AS results
+
+FROM  Users u
+join MovieRating m
+on u.user_id=m.user_id
+group by u.user_id,u.name
+order by count(*) desc,
+u.name
+limit 1
+)
+
+UNION ALL
+(
+select m.title as results
+from Movierating m1
+join Movies m
+on m1.movie_id=m.movie_id
+where m1.created_at >='2020-02-01'
+and m1.created_at< '2020-03-01'
+group by m.movie_id,m.title
+order by avg(m1.rating) desc,
+m.title
+limit 1
+);
