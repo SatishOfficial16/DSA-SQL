cd9aeb39400bd9d273e7e3300e5965b042f00893
SatishThota16 <satishnaidu3541@gmail.com>
2026-09-16 16:35:09 +0530

    feat(easy): add list-the-products-ordered-in-a-period solution [SQL]

---
+++ leetcode/easy/1327-list-the-products-ordered-in-a-period/solution.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+select p.product_name,sum(o.unit)
+from Products p 
+join Orders o
+on p.product_id=o.product_id
+and month(o.order_date)=2 
+group by p.product_id
+having sum(o.unit)>=100
