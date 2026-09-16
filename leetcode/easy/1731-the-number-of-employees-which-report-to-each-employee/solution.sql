e9a9f5c1f0c8f36d104862d74cdf92fb6c97fc9e
SatishThota16 <satishnaidu3541@gmail.com>
2026-09-16 20:30:03 +0530

    feat(easy): add the-number-of-employees-which-report-to-each-employee solution [SQL]

--- leetcode/easy/1731-the-number-of-employees-which-report-to-each-employee/solution.sql
+++ leetcode/easy/1731-the-number-of-employees-which-report-to-each-employee/solution.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-select e.employee_id,e.name,count(e1.reports_to)as reports_count,ceil(avg(e1.age))as average_age
+select e.employee_id,e.name,count(e1.reports_to)as reports_count,round(avg(e1.age),0)as average_age
 from Employees e join
  Employees e1
  on e.employee_id =e1.reports_to
