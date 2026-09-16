0709bba65f5e1cc7eeab62f8ae417fa727ad5657
SatishThota16 <satishnaidu3541@gmail.com>
2026-09-16 15:11:38 +0530

    feat(hard): add department-top-three-salaries solution [SQL]

---
+++ leetcode/hard/0185-department-top-three-salaries/solution.sql
@@ -0,0 +1,13 @@
+# Write your MySQL query statement below
+select Department,Employee,Salary
+from
+(select d.name as department,
+e.name as Employee,
+e.salary as Salary,
+dense_rank()over(partition by d.name order by e.salary desc)as rnk
+from Employee e
+join Department d
+on e.departmentId=d.id
+) as temp
+where rnk<=3
+
