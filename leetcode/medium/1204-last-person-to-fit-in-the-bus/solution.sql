6a658da5b7b89fba6680ea93f8a0a5ec6afe89b4
SatishThota16 <satishnaidu3541@gmail.com>
2026-09-16 17:13:15 +0530

    feat(medium): add last-person-to-fit-in-the-bus solution [SQL]

---
+++ leetcode/medium/1204-last-person-to-fit-in-the-bus/solution.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+select person_name
+from (
+    select *,
+    sum(weight) over(order by turn)as total 
+    from Queue
+)temp
+where total<=1000
+order by turn desc
+limit 1
